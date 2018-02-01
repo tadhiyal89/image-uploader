ba20a39ecb00c9fce456e3c8e4a1d2b79fb77f9a
IMPETUS\mohit.tadhiyal <mohit.tadhiyal@impetus.co.in>
2018-02-01 21:27:18 +0530

    test

--- server.js
+++ server.js
@@ -38,13 +38,4 @@ var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port);
-   let imgArr=[];
-	
-   fs.readdir('./public/img', (err, files) => {
-	files.forEach(file => {
-		imgArr.push(file)
-	  });
-	  console.log(JSON.stringify(imgArr));
-	});
-	
 })
